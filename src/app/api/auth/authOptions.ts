96da67d3c80017bf6793df56d6fd8570ab7dcd67
bantheus <matheuschmidt.b@gmail.com>
2024-04-24 10:47:10 -0300

    ✨ Feat: add next auth config

---
+++ src/app/api/auth/authOptions.ts
@@ -0,0 +1,16 @@
+import { db } from "@/lib/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  secret: process.env.SECRET,
+};
