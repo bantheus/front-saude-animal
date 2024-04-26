10dbe7a9119a03e34da52a573ca0f665c5bd5843
bantheus <matheuschmidt.b@gmail.com>
2024-04-26 17:15:18 -0300

    ✨ Feat: add quick search function

--- src/components/quick-search/quickSearchIcon.tsx
+++ src/components/quick-search/quickSearchIcon.tsx
@@ -1,13 +1,32 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
 
 interface QuickSearchIconProps {
   icon: string;
   title: string;
 }
 
+interface AnimalSearchForm {
+  title: string;
+}
+
 const QuickSearchIcon = ({ icon, title }: QuickSearchIconProps) => {
+  const router = useRouter();
+
+  const { handleSubmit } = useForm<AnimalSearchForm>();
+
+  const onSubmit = () => {
+    router.push(`/animais/busca?especie=${title}`);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center gap-y-2">
+    <div
+      onClick={() => handleSubmit(onSubmit)()}
+      className="flex flex-col items-center justify-center gap-y-2"
+    >
       <Image
         width={32}
         height={32}
